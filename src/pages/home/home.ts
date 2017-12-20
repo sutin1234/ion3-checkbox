89e286ead29e0bd7cbdccfc9b9caef0267be24c1
sutin <tony.sutin1234@gmail.com>
2017-12-20 13:31:56 +0700

    add ionic3 tootle checkbox
    add ionic3 tootle checkbox

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items = [];
+  constructor(public navCtrl: NavController) {}
+  ionViewDidLoad() {
+    this.items = [
+      { name: "Dancing", checked: false },
+      { name: "Jazz", checked: false },
+      { name: "Metal", checked: false },
+      { name: "Pop", checked: false },
+      { name: "Rock", checked: false },
+      { name: "Folk Metal", checked: false }
+    ];
   }
-
+  updateCucumber(){
+    this.items.forEach((element) => {
+      element.checked == true ? element.checked = false : element.checked =  true;
+    });
+  }
 }
